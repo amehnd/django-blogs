1b75cc5f73af7f1bbc465db119074150e1b83f56
Aryan Mehndiratta <aryanmehndiratta166@gmail.com>
2024-12-02 00:05:16 +0000

    defined model and installed pillow

--- blogger/blogs/models.py
+++ blogger/blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Blogs(model.Model);
+ user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+text = models.TextField(max_length=200)
+pic = models.ImageField(upload_to = 'pics/', blank=True, null= True)
+created_at = models.DateTimeField(auto_now_add=True)
+updated_at = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    return f'{self.user.username} - {self.text[:10]}'
+    
+
+
